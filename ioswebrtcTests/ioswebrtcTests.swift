4601b3ffe351d905a518c5ac425b52d25af340ab
tdtsh <systemsfailing@gmail.com>
2018-02-21 09:55:19 +0900

    Initial Commit

---
+++ ioswebrtcTests/ioswebrtcTests.swift
@@ -0,0 +1,36 @@
+//
+//  ioswebrtcTests.swift
+//  ioswebrtcTests
+//
+//  Created by tdtsh on 2018/02/21.
+//  Copyright © 2018年 tdtsh. All rights reserved.
+//
+
+import XCTest
+@testable import ioswebrtc
+
+class ioswebrtcTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
