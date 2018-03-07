c0a88424f5df0cb725cb90b718aae395179f1388
tdtsh <systemsfailing@gmail.com>
2018-03-07 20:02:15 +0900

    add connect button

--- ioswebrtc/DetailViewController.swift
+++ ioswebrtc/DetailViewController.swift
@@ -22,6 +22,28 @@ class DetailViewController: UIViewController {
     // ナビゲーションのタイトル変更
     self.navigationItem.title = "iOS WebRTC detail"
 
+    renderView()
+  }
+
+  // Viewを描画する
+  func renderView() {
+    print("Detail:", #function, #line, "start")
+
+    // Connectボタン作成
+    let buttonConnect = UIButton(frame: CGRect.init(x: 0, y: 0, width: self.view.frame.size.width / 3, height: 50))
+    buttonConnect.backgroundColor = UIColor.init(red: 0.2, green: 0.3, blue: 0.7, alpha: 1)
+
+    // 角丸
+    buttonConnect.layer.cornerRadius = 5.0
+    buttonConnect.layer.masksToBounds = true
+
+    // タイトルとか
+    buttonConnect.setTitle("Connect", for: UIControlState.normal)
+
+    // 配置
+    buttonConnect.layer.position = CGPoint(x: self.view.bounds.width/4, y:self.view.bounds.height - 60)
+
+    self.view.addSubview(buttonConnect)
   }
 
   override func didReceiveMemoryWarning() {
