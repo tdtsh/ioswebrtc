76b40339ec1c92555e5e6eaffca8edd927e2b912
tdtsh <systemsfailing@gmail.com>
2018-03-08 10:01:47 +0900

    add hungup button

--- ioswebrtc/DetailViewController.swift
+++ ioswebrtc/DetailViewController.swift
@@ -32,18 +32,20 @@ class DetailViewController: UIViewController {
     // Connectボタン作成
     let buttonConnect = UIButton(frame: CGRect.init(x: 0, y: 0, width: self.view.frame.size.width / 3, height: 50))
     buttonConnect.backgroundColor = UIColor.init(red: 0.2, green: 0.3, blue: 0.7, alpha: 1)
-
-    // 角丸
     buttonConnect.layer.cornerRadius = 5.0
     buttonConnect.layer.masksToBounds = true
-
-    // タイトルとか
     buttonConnect.setTitle("Connect", for: UIControlState.normal)
-
-    // 配置
     buttonConnect.layer.position = CGPoint(x: self.view.bounds.width/4, y:self.view.bounds.height - 60)
-
     self.view.addSubview(buttonConnect)
+
+    // Hangupボタン作成
+    let buttonHangup = UIButton(frame: CGRect.init(x: 0, y: 0, width: self.view.frame.size.width / 3, height: 50))
+    buttonHangup.backgroundColor = UIColor.init(red: 0.7, green: 0.3, blue: 0.1, alpha: 1)
+    buttonHangup.layer.cornerRadius = 5.0
+    buttonHangup.layer.masksToBounds = true
+    buttonHangup.setTitle("Hangup", for: UIControlState.normal)
+    buttonHangup.layer.position = CGPoint(x: (self.view.bounds.width/4)*3, y:self.view.bounds.height - 60)
+    self.view.addSubview(buttonHangup)
   }
 
   override func didReceiveMemoryWarning() {
