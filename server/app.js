24d17f8d2aee4d79e5b8baeccfdaf040cc896d28
tdtsh <systemsfailing@gmail.com>
2018-03-27 23:25:27 +0900

    install express

--- server/app.js
+++ server/app.js
@@ -1,7 +1,24 @@
 'use strict';
 
-const WebSocketServer = require('ws').Server;
+const express = require('express');
+const path = require('path');
+const app = express();
+const ECT = require('ect');
+const ectRenderer = ECT({
+  watch: true,
+  root: __dirname + '/views',
+  ext: '.ect'
+});
+app.set('view engine', 'ect');
+app.engine('ect', ectRenderer.render);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const routes = require('./routes/index');
+app.use('/', routes);
+
 const port = 4000;
+/*const WebSocketServer = require('ws').Server;
 const wsServer = new WebSocketServer({
   port: port
 });
@@ -17,7 +34,17 @@ wsServer.on('connection', (ws) => {
       }
     });
   });
-});
+});*/
 
 console.log(`server started. port:${port}`);
+const server = app.listen(port, () => {
+  return console.log(`Express server listening on port ${port}`);
+});
 
+const io = require('socket.io')(server);
+io.sockets.on('connection', (socket) => {
+  console.log('connected');
+  socket.on('message', (message) => {
+    socket.broadcast.emit('message'. message);
+  });
+});
---
+++ server/routes/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  res.render('index', {
+    title: 'ioswebrtc index',
+    body: 'サンプルページ'
+  });
+});
+
+module.exports = router;
